bcc421c58ea6df294dec9f15cb0a4b1d076a7a2e
gravo-lan <alvintsolh@gmail.com>
2024-01-24 09:01:32 +0800

    Finished

--- src/ClassSection.java
+++ src/ClassSection.java
@@ -1,2 +1,67 @@
+import java.util.ArrayList;
+
 public class ClassSection {
+
+    private String subject;
+    private int capacity, yearLevel;
+    private final ArrayList<Student> students;
+
+    public ClassSection(String subject, int capacity, int yearLevel) {
+        this.subject = subject;
+        this.capacity = capacity;
+        this.yearLevel = yearLevel;
+        this.students = new ArrayList<>();
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getYearLevel() {
+        return yearLevel;
+    }
+
+    public void setYearLevel(int yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        if (student.getYearLevel() == yearLevel) {
+            for (Student s : students) {
+                if (s.equals(student) || s.getId() == student.getId()) {
+                    return;
+                }
+            }
+            students.add(student);
+        }
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+    }
+
+    public boolean isStudentEnrolled(Student student) {
+        return students.contains(student);
+    }
+
+    @Override
+    public String toString() {
+        return "ClassSection{subject='" + subject + "', capacity=" + capacity + ", students=" + students + "}";
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,71 @@
+import java.util.Objects;
+
 public class Student {
+    private int age, yearLevel, id;
+    private static int studentCount;
+    private String firstName, lastName;
+
+    public Student(String firstName, String lastName, int age, int yearLevel) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearLevel = yearLevel;
+        this.id=studentCount++;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getYearLevel() {
+        return yearLevel;
+    }
+
+    public void setYearLevel(int yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStudentCount() {
+        return studentCount;
+    }
+
+    public boolean equals(Student other) {
+        return Objects.equals(firstName, other.getFirstName()) &&
+                Objects.equals(lastName, other.getLastName()) &&
+                age == other.getAge() &&
+                yearLevel == other.getYearLevel();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", firstName='" + firstName + "', lastName='" + lastName + "', age=" + age + "}";
+    }
 }
